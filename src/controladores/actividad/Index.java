4b4ce1475bba078be351fdc980a8c13cb696d240
Agustin Silva <silva.agustinn@gmail.com>
2016-04-13 18:12:43 -0300

    Corrije el formulario de nuevo usuario

---
+++ src/controladores/actividad/Index.java
@@ -0,0 +1,34 @@
+package controladores.actividad;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/actividades")
+public class Index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		RequestDispatcher rs = request.getRequestDispatcher("/vistas/Actividades/index.jsp"); 
+		rs.forward(request, response);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.sendRedirect("/vistas/Actividades/index.jsp");
+		processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.sendRedirect("/vistas/Actividades/index.jsp");
+		processRequest(request, response);
+		
+	}
+
+}
