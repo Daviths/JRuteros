b1ed0043e6b6897bbf4d950a7c500d755e92c376
David <davidelga47@hotmail.com>
2016-05-11 17:45:11 -0300

    Añadida clase de Puntaje

---
+++ src/modelos/Puntaje.java
@@ -0,0 +1,20 @@
+package modelos;
+
+public class Puntaje {
+	//Variables
+	private Integer puntos;
+	private String fecha;
+	//Metodos
+	public Integer getPuntos() {
+		return puntos;
+	}
+	public void setPuntos(Integer puntos) {
+		this.puntos = puntos;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+}
